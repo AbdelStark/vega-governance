e9f1b7d6aeb99a8d9aa1dae7d7808ff6697a3426
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2020-12-16 11:17:52 +0100

    inti end to end test

--- src/services/VegaWallet.js
+++ src/services/VegaWallet.js
@@ -1,24 +1,112 @@
 const axios = require("axios");
+import {StatusCodes} from 'http-status-codes';
 
 export default class VegaWallet {
+
+
     constructor(endpoint) {
         this.endpoint = endpoint;
-        this.endpointLogin = `${endpoint}/api/v1/auth/token`
+        this.endpointCreateWallet = `${endpoint}/api/v1/wallets`;
+        this.endpointLogin = `${endpoint}/api/v1/auth/token`;
+        this.endpointLogout = this.endpointLogin;
+        this.endpointListKeys = `${endpoint}/api/v1/keys`;
+        this._token = null;
     }
 
-    async login(wallet, passphrase){
-        try{
+    /**
+     * Creates new wallet.
+     * @param wallet {String}
+     * @param passphrase {String}
+     * @returns {Promise<boolean>}
+     */
+    async createWallet(wallet, passphrase) {
+        return this.loginOrCreate(true, wallet, passphrase);
+    }
+
+    /**
+     * Logins to existing wallet.
+     * @param wallet {String}
+     * @param passphrase {String}
+     * @returns {Promise<boolean>}
+     */
+    async login(wallet, passphrase) {
+        return this.loginOrCreate(false, wallet, passphrase);
+    }
+
+    /**
+     *
+     * @param create {boolean}
+     * @param wallet {String}
+     * @param passphrase {String}
+     * @returns {Promise<boolean>}
+     */
+    async loginOrCreate(create, wallet, passphrase){
+        try {
             const response = await axios.post(
-                this.endpointLogin,
+                create ? this.endpointCreateWallet : this.endpointLogin,
                 {
                     wallet: wallet,
                     passphrase: passphrase,
                 }
             );
-            return response.data;
-        }catch (e) {
+            if (response.status !== StatusCodes.OK) {
+                return false;
+            }
+            this.token = response.data.token;
+            return true;
+        } catch (e) {
             console.error('authentication failed: ', e);
             throw e;
         }
+    }
+
+    async listKeys() {
+        try {
+            const response = await axios.get(
+                this.endpointListKeys,
+                this.authenticated(),
+            );
+            return response.data.keys;
+        } catch (e) {
+            console.error('logout failed: ', e);
+            throw e;
+        }
+    }
+
+    async logout() {
+        try {
+            const response = await axios.delete(
+                this.endpointLogout,
+                this.authenticated(),
+            );
+            if (response.status !== StatusCodes.OK) {
+                return false;
+            }
+            this.token = null;
+            return true;
+        } catch (e) {
+            console.error('logout failed: ', e);
+            throw e;
+        }
+    }
+
+    authenticated() {
+        return {
+            headers: this.authorizationHeader(),
+        }
+    }
+
+    authorizationHeader() {
+        return {
+            'Authorization': `Bearer ${this.token}`,
+        }
+    }
+
+    get token() {
+        return this._token;
+    }
+
+    set token(value) {
+        this._token = value;
     }
 }
--- test/vega/wallet.js
+++ test/vega/wallet.js
@@ -6,10 +6,19 @@ const walletEndpoint = 'http://127.0.0.1:1789';
 const vegaWallet = new VegaWallet(walletEndpoint);
 
 describe('Vega Wallet', function () {
-    describe('#login()', function () {
-        it.only('should authenticate', async function () {
-            const response = await vegaWallet.login('gitcoinvega', 'gitcoin');
-            console.log(response);
+    describe('#endtoend()', function () {
+        it.only('should work', async function () {
+            const now = Date.now().toString();
+            const wallet = `wallet-${now}`;
+            const passphrase = `passphrase-${now}`;
+            expect(await vegaWallet.createWallet(wallet, passphrase)).to.be.true;
+            expect(await vegaWallet.login(wallet, passphrase)).to.be.true;
+            expect(vegaWallet.token).not.to.be.null;
+            let keys = await vegaWallet.listKeys();
+            expect(keys).not.to.be.null;
+            expect(keys).to.be.an('array').that.is.empty;
+            expect(await vegaWallet.logout()).to.be.true;
+            expect(vegaWallet.token).to.be.null;
         });
     });
 });
