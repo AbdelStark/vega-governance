8017650fe7e575d38a5f768533a489eda54a6ba7
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2020-12-16 11:43:33 +0100

    add tests

--- src/services/VegaWallet.js
+++ src/services/VegaWallet.js
@@ -4,41 +4,46 @@ import {StatusCodes} from 'http-status-codes';
 export default class VegaWallet {
 
 
+    /**
+     * Build a new Vega Wallet service.
+     * @param endpoint {String} Root URL of the wallet.
+     */
     constructor(endpoint) {
         this.endpoint = endpoint;
         this.endpointCreateWallet = `${endpoint}/api/v1/wallets`;
         this.endpointLogin = `${endpoint}/api/v1/auth/token`;
         this.endpointLogout = this.endpointLogin;
+        this.endpointGenerateKeypair = `${endpoint}/api/v1/keys`;
         this.endpointListKeys = `${endpoint}/api/v1/keys`;
         this._token = null;
     }
 
     /**
-     * Creates new wallet.
-     * @param wallet {String}
-     * @param passphrase {String}
-     * @returns {Promise<boolean>}
+     * Create new wallet.
+     * @param wallet {String} The wallet identifier.
+     * @param passphrase {String} The wallet passphrase.
+     * @returns {Promise<boolean>} true if success, false otherwise
      */
     async createWallet(wallet, passphrase) {
         return this.loginOrCreate(true, wallet, passphrase);
     }
 
     /**
-     * Logins to existing wallet.
-     * @param wallet {String}
-     * @param passphrase {String}
-     * @returns {Promise<boolean>}
+     * Login to existing wallet.
+     * @param wallet {String} The wallet identifier.
+     * @param passphrase {String} The wallet passphrase.
+     * @returns {Promise<boolean>} true if success, false otherwise
      */
     async login(wallet, passphrase) {
         return this.loginOrCreate(false, wallet, passphrase);
     }
 
     /**
-     *
-     * @param create {boolean}
-     * @param wallet {String}
-     * @param passphrase {String}
-     * @returns {Promise<boolean>}
+     * Create new wallet or login to existing wallet.
+     * @param create {boolean} true if wallet creation requested, false otherwise.
+     * @param wallet {String} The wallet identifier.
+     * @param passphrase {String} The wallet passphrase.
+     * @returns {Promise<boolean>} true if success, false otherwise
      */
     async loginOrCreate(create, wallet, passphrase){
         try {
@@ -55,11 +60,15 @@ export default class VegaWallet {
             this.token = response.data.token;
             return true;
         } catch (e) {
-            console.error('authentication failed: ', e);
+            console.error('loginOrCreate failed: ', e);
             throw e;
         }
     }
 
+    /**
+     * List keys managed by the wallet.
+     * @returns {Promise<*>}
+     */
     async listKeys() {
         try {
             const response = await axios.get(
@@ -73,6 +82,43 @@ export default class VegaWallet {
         }
     }
 
+    /**
+     * Generate new keypair associated to optional meta data.
+     * @param passphrase The wallet passphrase.
+     * @param meta {Map<String,String>} Meta data associated to the key.
+     * @returns {Promise<*>}
+     */
+    async generateKeypair(passphrase, meta = null){
+        try {
+            const requestPayload = {
+                passphrase: passphrase,
+            }
+            if(meta !== null && meta.size > 0){
+                const keyMetaData = [];
+                meta.forEach((value, key) => {
+                    keyMetaData.push({
+                       key: key,
+                       value: value,
+                    });
+                });
+                requestPayload.meta = keyMetaData;
+            }
+            const response = await axios.post(
+                this.endpointGenerateKeypair,
+                requestPayload,
+                this.authenticated(),
+            );
+            return response.data;
+        } catch (e) {
+            console.error('generateKeypair failed: ', e);
+            throw e;
+        }
+    }
+
+    /**
+     * Logout to the wallet.
+     * @returns {Promise<boolean>} true if success, false otherwise.
+     */
     async logout() {
         try {
             const response = await axios.delete(
--- test/vega/wallet.js
+++ test/vega/wallet.js
@@ -11,14 +11,57 @@ describe('Vega Wallet', function () {
             const now = Date.now().toString();
             const wallet = `wallet-${now}`;
             const passphrase = `passphrase-${now}`;
+
+            // Create new wallet
+            console.log('create new wallet');
             expect(await vegaWallet.createWallet(wallet, passphrase)).to.be.true;
+
+            // Login to wallet
+            console.log('login to wallet');
             expect(await vegaWallet.login(wallet, passphrase)).to.be.true;
             expect(vegaWallet.token).not.to.be.null;
+
+            // Get list of wallet keys (must be empty)
+            console.log('list keys (must be empty)');
             let keys = await vegaWallet.listKeys();
             expect(keys).not.to.be.null;
             expect(keys).to.be.an('array').that.is.empty;
+
+            // Generate new key pair
+            console.log('generate  new key pair');
+            let generateKeypairResponse = await vegaWallet.generateKeypair(passphrase);
+            expect(generateKeypairResponse).to.have.property('key');
+            assertKeypair(generateKeypairResponse.key);
+
+            // Get list of wallet keys (must contain 1 key)
+            console.log('list keys (must contain 1 key)');
+            keys = await vegaWallet.listKeys();
+            expect(keys).not.to.be.null;
+            expect(keys).to.be.an('array').that.has.lengthOf(1);
+            assertKeypair(keys[0]);
+
+            // Generate new key pair (with meta data)
+            console.log('generate new key pair (with meta data)');
+            const meta = new Map();
+            meta.set('meta-key1', 'meta-value1');
+            meta.set('meta-key2', 'meta-value2');
+            generateKeypairResponse = await vegaWallet.generateKeypair(passphrase, meta);
+            expect(generateKeypairResponse).to.have.property('key');
+            assertKeypair(generateKeypairResponse.key, 2);
+
+            // Logout
             expect(await vegaWallet.logout()).to.be.true;
             expect(vegaWallet.token).to.be.null;
         });
     });
 });
+
+function assertKeypair(key, expectedMetaDataSize = 0) {
+    expect(key).to.have.property('pub');
+    expect(key).to.have.property('algo');
+    expect(key).to.have.property('tainted');
+    expect(key).to.have.property('meta');
+    if (expectedMetaDataSize !== 0) {
+        expect(key.meta).to.be.an('array').that.has.lengthOf(expectedMetaDataSize);
+    }
+}
