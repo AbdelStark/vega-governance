269683a79e442fa993e7027558cc2d0e5da6092a
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2020-12-16 10:26:15 +0100

    init project

---
+++ src/main.js
@@ -0,0 +1,88 @@
+/*
+ =========================================================
+ * Vue Black Dashboard - v1.1.0
+ =========================================================
+
+ * Product Page: https://www.creative-tim.com/product/black-dashboard
+ * Copyright 2018 Creative Tim (http://www.creative-tim.com)
+
+ =========================================================
+
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+ */
+import Vue from "vue";
+import './plugins/bootstrap-vue';
+import './plugins/font-awesome';
+import './plugins/clipboard';
+import Vuex from "vuex";
+import VueRouter from "vue-router";
+import RouterPrefetch from 'vue-router-prefetch';
+import App from "./App";
+import router from "./router/index";
+
+import BlackDashboard from "./plugins/blackDashboard";
+import i18n from "./i18n";
+import './registerServiceWorker';
+import VegaWallet from "@/services/VegaWallet";
+
+Vue.use(BlackDashboard);
+Vue.use(VueRouter);
+Vue.use(RouterPrefetch);
+Vue.use(Vuex);
+
+const settings = buildSettings();
+const services = buildServices(settings);
+// create store
+const store = new Vuex.Store({
+  state: {
+    settings: settings,
+    services: services,
+  },
+  mutations: {}
+});
+
+/* eslint-disable no-new */
+new Vue({
+  router,
+  i18n,
+  render: h => h(App),
+  store,
+}).$mount("#app");
+
+router.push({path: '/'});
+
+function buildSettings() {
+  if(localStorage.getItem('settings')){
+    return settingsFromLocalStorage();
+  }
+  return defaultSettings();
+}
+
+function settingsFromLocalStorage(){
+  console.log('retrieving settings from local storage');
+  try {
+    return  JSON.parse(localStorage.getItem('settings'));
+  } catch(e) {
+    console.error(e);
+    localStorage.removeItem('settings');
+    return defaultSettings();
+  }
+}
+
+function defaultSettings(){
+  console.log('loading default application settings');
+  return {
+    vega: {
+      wallet: {
+        endpoint: 'http://127.0.0.1',
+      }
+    }
+  };
+}
+
+function buildServices(settings) {
+  return {
+    vegaWallet: new VegaWallet(settings.vega.wallet.endpoint),
+  };
+}
---
+++ src/services/VegaWallet.js
@@ -0,0 +1,5 @@
+export default class VegaWallet {
+    constructor(endpoint) {
+        this.endpoint = endpoint;
+    }
+}
